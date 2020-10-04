802bf0d530d39e061209a24f28598bc4d9bfe1f9
Payrav <payrav-343@mail.ru>
2020-10-04 23:03:35 +0500

    Добавил функцию Pay и FindPaymentByID

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -8,9 +8,46 @@ import (
 var (
 	ErrPhoneRegistered      = errors.New("телефон уже зарегистрирован")         
 	ErrAmountMustBePositive = errors.New("сумма должна быть больше нуля") 
-	ErrAccountNotFound      = errors.New("аккаунт не найден")                
+	ErrAccountNotFound      = errors.New("аккаунт не найден")
+	ErrPaymentNotFound = errors.New("Платеж не найден")                
 )
 
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	if account.Balance < amount {
+		return nil, ErrNotEnoughtBalance
+	}
+
+	account.Balance -= amount
+
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID:        paymentID,
+		AccountID: accountID,
+		Amount:    amount,
+		Category:  category,
+		Status:    types.PaymentStatusInProgress,
+	}
+
+	s.payments = append(s.payments, payment)
+	return payment, nil
+}
 
 type Service struct {
 	nextAccountID int64
@@ -64,4 +101,33 @@ func (s *Service) Deposit(accountID int64, amount types.Money) error {
 
 	account.Balance += amount
 	return nil
+}
+
+
+
+func (s *Service) FindPaymentByID(paymentID string)(*types.Payment,error){
+	var payment *types.Payment
+	for _, pay_ment := range s.payments {
+		if pay_ment.ID== paymentID{
+			payment =pay_ment
+		}
+	}
+	if payment == nil{
+		return nil,ErrPaymentNotFound
+	}
+	return payment,nil
+}
+
+func (s *Service) Reject(paymentID string) error{
+	pay_ment,err:=s.FindPaymentByID(paymentID)
+	if err != nil{
+		return err
+	}
+
+	acc, err := s.FindAccountByID(pay_ment.ID)
+	if err != nil{
+		return err
+	}
+	pay_ment.Status = types.PaymentStatusFail
+	acc.Balance += pay_ment.Amount
 }
