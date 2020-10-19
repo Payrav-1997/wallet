823c949730e41ac8510bc50ff45ad80d88f0031d
Payrav <payrav-343@mail.ru>
2020-10-20 02:07:51 +0500

    Добавил тест

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,29 +1,50 @@
 package sum
 
-import "testing"
+import (
+	
+	"testing"
 
-func BenchmarkReagular(b *testing.B) {
-	want:= int64(200)
+	"github.com/Payrav-1997/wallet/pkg/types"
+)
+
+// func BenchmarkReagular(b *testing.B) {
+// 	want:= int64(200)
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		result:= Regular()
+// 		b.StopTimer()
+// 		if result!=want{
+// 			b.Fatalf("Invalid result got %v,want %v",result,want)
+// 		}
+// 		b.StartTimer()
+// 	}
+// }
+
+// func BenchmarkConcurrently(b *testing.B) {
+// 	want:= int64(200)
+// 	b.ResetTimer()
+// 	for i := 0; i < b.N; i++ {
+// 		result:= Concurrently()
+// 		b.StopTimer()
+// 		if result!=want{
+// 			b.Fatalf("Invalid result got %v,want %v",result,want)
+// 		}
+// 		b.StartTimer()
+// 	}
+// }
+
+func BenchmarkSumPayments(b *testing.B){
+	want := types.Money(0)
 	b.ResetTimer()
+	var service Service
 	for i := 0; i < b.N; i++ {
-		result:= Regular()
+		result := service.SumPayments(2)
 		b.StopTimer()
-		if result!=want{
-			b.Fatalf("Invalid result got %v,want %v",result,want)
+		if result != types.Money(want) {
+			b.Fatalf("Invalid result got %v,want %v", result, want)
 		}
 		b.StartTimer()
 	}
 }
 
-func BenchmarkConcurrently(b *testing.B) {
-	want:= int64(200)
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		result:= Concurrently()
-		b.StopTimer()
-		if result!=want{
-			b.Fatalf("Invalid result got %v,want %v",result,want)
-		}
-		b.StartTimer()
-	}
-}
+
