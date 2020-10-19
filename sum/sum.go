7f6e10b53de66d9e0c9f2926fca94c3f1e025d85
Payrav <payrav-343@mail.ru>
2020-10-20 02:21:57 +0500

    Изменил SumPayments

--- sum/sum.go
+++ sum/sum.go
@@ -14,81 +14,71 @@ type Service struct {
 	favorites     []*types.Favorite
 }
 
-// func Regular() int64 {
-// sum := int64(0)
-// for i := 0; i < 200; i++ {
-// 	sum++
-// }
-// return sum
-// }
+func Regular() int64 {
+sum := int64(0)
+for i := 0; i < 200; i++ {
+	sum++
+}
+return sum
+}
 
-// func Concurrently() int64 {
-// wg := sync.WaitGroup{}
-// wg.Add(2)
-// mu := sync.Mutex{}
-// sum := int64(0)
-// go func() {
-// 	defer wg.Done()
-// 	val := int64(0)
-// 	for i := 0; i < 100; i++ {
-// 		val++
-// 	}
-// 	mu.Lock()
-// 	defer mu.Unlock()
-// 	sum += val
+func Concurrently() int64 {
+wg := sync.WaitGroup{}
+wg.Add(2)
+mu := sync.Mutex{}
+sum := int64(0)
+go func() {
+	defer wg.Done()
+	val := int64(0)
+	for i := 0; i < 100; i++ {
+		val++
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	sum += val
 
-// }()
-// go func() {
-// 	defer wg.Done()
-// 	val := int64(0)
-// 	for i := 0; i < 100; i++ {
-// 		val++
-// 	}
-// 	mu.Lock()
-// 	defer mu.Unlock()
-// 	sum += val
+}()
+go func() {
+	defer wg.Done()
+	val := int64(0)
+	for i := 0; i < 100; i++ {
+		val++
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	sum += val
 
-// }()
+}()
 
-// wg.Wait()
-// return sum
-// }
+wg.Wait()
+return sum
+}
 
-func (s *Service) SumPayments(gouroutines int) types.Money {
+func (s *Service)SumPayments(goroutines int) types.Money {
+	
 	wg := sync.WaitGroup{}
-	mu := sync.Mutex{}
-	sum := int64(0)
-	sum1 := len(s.payments) / gouroutines
-	if gouroutines == 0 {
-		sum1 = len(s.payments)
-	}
-	for i := 0; i < gouroutines-1; i++ {
-		wg.Add(1)
-		go func(num int) {
-			defer wg.Done()
-			val := int64(0)
-			payment := s.payments[:(i+1)*sum1]
-			for _, pay := range payment {
-				val += int64(pay.Amount)
-			}
-			mu.Lock()
-			defer mu.Unlock()
-			sum += val
-		}(i)
-		num := 0
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			val := int64(0)
-			payment := s.payments[num*sum1:]
-			for _, pay := range payment {
-				val += int64(pay.Amount)
-			}
-			mu.Lock()
-			defer mu.Unlock()
-			sum += val
-		}()
-	}
+	wg.Add(goroutines) 
+	mu := sync.Mutex{} 
+	var sum types.Money
+
+	go func(){
+		wg.Done()
+		for _, payment := range s.payments {
+			pay := payment
+			sum += pay.Amount	
+		}
+		mu.Lock()
+		defer mu.Unlock()
+	}()
+	go func(){
+	    wg.Done() 
+		for _, payment := range s.payments {
+			pay := payment
+			sum += pay.Amount	
+		}
+		mu.Lock()
+		defer mu.Unlock()
+	}()
 	wg.Wait()
-	return types.Money(sum)
+	return sum
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -7,31 +7,31 @@ import (
 	"github.com/Payrav-1997/wallet/pkg/types"
 )
 
-// func BenchmarkReagular(b *testing.B) {
-// 	want:= int64(200)
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		result:= Regular()
-// 		b.StopTimer()
-// 		if result!=want{
-// 			b.Fatalf("Invalid result got %v,want %v",result,want)
-// 		}
-// 		b.StartTimer()
-// 	}
-// }
+func BenchmarkReagular(b *testing.B) {
+	want:= int64(200)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		result:= Regular()
+		b.StopTimer()
+		if result!=want{
+			b.Fatalf("Invalid result got %v,want %v",result,want)
+		}
+		b.StartTimer()
+	}
+}
 
-// func BenchmarkConcurrently(b *testing.B) {
-// 	want:= int64(200)
-// 	b.ResetTimer()
-// 	for i := 0; i < b.N; i++ {
-// 		result:= Concurrently()
-// 		b.StopTimer()
-// 		if result!=want{
-// 			b.Fatalf("Invalid result got %v,want %v",result,want)
-// 		}
-// 		b.StartTimer()
-// 	}
-// }
+func BenchmarkConcurrently(b *testing.B) {
+	want:= int64(200)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		result:= Concurrently()
+		b.StopTimer()
+		if result!=want{
+			b.Fatalf("Invalid result got %v,want %v",result,want)
+		}
+		b.StartTimer()
+	}
+}
 
 func BenchmarkSumPayments(b *testing.B){
 	want := types.Money(0)
